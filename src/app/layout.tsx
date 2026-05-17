7107d9d13063c83db7d474065eeaf9180bb7031f
spireteam98 <spireteam98@gmail.com>
2026-05-17 07:15:04 +0300

    Finish merge from origin/main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type {Metadata} from 'next';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/next';
 import { FirebaseClientProvider } from '@/firebase/client-provider';
 import { Toaster } from '@/components/ui/toaster';
 
@@ -71,6 +72,7 @@ export default function RootLayout({
         <FirebaseClientProvider>
           {children}
           <Toaster />
+          <Analytics />
         </FirebaseClientProvider>
       </body>
     </html>
